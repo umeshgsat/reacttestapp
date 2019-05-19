b647ad5612c28e2319ad1d3ab529cba2649f6bab
umesh <umeshgsat@gmail.com>
2019-05-19 11:29:36 +0530

    removing lreact web ink

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -10,14 +10,7 @@ function App() {
         <p>
           React webapp hosted.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
       </header>
     </div>
   );
